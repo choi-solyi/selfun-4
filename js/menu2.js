309b3fd098eab3292ce51a4840c122e027dc4cc8
unknown <gung4060@gmail.com>
2019-12-17 21:19:40 +0900

    menu2-10

---
+++ js/menu2.js
@@ -0,0 +1,30 @@
+$('li').on('click', function () {
+    $('li').children().children().filter('img').css('border', '0px solid #7b94ff');
+    $('li').children().children().filter('p').css('color', 'black');
+    $(this).children().children().filter('img').css('border', '2px solid #7b94ff');
+    $(this).children().children().filter('p').css('color', '#7b94ff');
+});
+$('#chevronleft').on('click', function () {
+
+    var $width = $('#category>li').outerWidth(true);
+
+    //$("li:last").insertBefore("li:first");
+    $("#category").css({
+        left: $width * -1 + "px"
+    });
+
+});
+
+
+
+$('#chevronright').on('click', function () {
+    var $width = $('#category>li').outerWidth(true);
+    $('#category').animate({
+        left: ($width * -1) + "px"
+    }, 300, 'swing', function () {
+        $("#category").children('li:first')
+            .insertAfter($("#category").children('li:last'));
+
+    });
+
+});
