e4a99019eb75dc41cd61d26782559dcfc8162a23
lbo1989 <lbo1989@naver.com>
2019-12-24 00:52:12 +0900

    my5 menu5 goods update! 191224

--- js/goods.js
+++ js/goods.js
@@ -19,3 +19,21 @@
 	};
 
 })(jQuery);
+
+
+
+jQuery(document).ready(function ($) {
+      $(document).load('ajax/footer.html', function(){
+
+        $('.footerlist').append(
+            "<li><a href='http://cloud0477.dothome.co.kr/'><img src='https://res.cloudinary.com/dgggcrkxq/image/upload/v1573198836/noticon/uydcemefupin4kvi5pii.gif'> 개발자 사이트</a></li>");
+
+        $('.footerlist').children(':last').css({
+            'margin':'10px'
+        });
+
+        $('.footerlist').children(':last').children('a').children('img').css({
+            'width':'10px'
+        });
+    });
+});
