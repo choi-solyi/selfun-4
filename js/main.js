579967a8c4ebeab9e2e9c02ab1e11fb986d8d6b3
Solyi Choi <choi-solyi@naver.com>
2019-12-23 20:56:51 +0900

    lastcommit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 $(document).ready(function () {
-    /*    $('.m1:nth-child(1)>h3>a')
-            .css({
-                'border-top': '2px solid #7b94ff',
-                'border-bottom': '2px solid #7b94ff',
-            }).parent().next().show();
-
-            $('li.m1>h3>a').on('click focus', function () {
-                        $('li.m1>h3>a')
-                            .css({
-                                'border-top': '1px solid silver',
-                                'border-bottom': '1px solid silver'
-                            }).parent().next('div').hide();
-            $(this).css({
-                'border-top':'2px solid #7b94ff',
-                'border-bottom':'2px solid #7b94ff'
-            }).parent().next().show();
-        });*/
-
     $.ajax({
         url: 'ranking.html',
 
@@ -54,9 +36,5 @@ $(document).ready(function () {
     });
 
 
-    /*$('div.mainFir').mouseenter(function(){
-        $(this).fadeOut(1000)
 
-
-    });*/
 });
