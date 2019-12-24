5f917fca30d6e8c65a36559f97d637b0fc09f8e4
unknown <lbo1989@naver.com>
2019-12-24 10:14:31 +0900

    goodshtml css commit

--- js/my5.js
+++ js/my5.js
@@ -22,14 +22,15 @@ jQuery(document).ready(function ($) {
       $(document).load('ajax/footer.html', function(){
 
         $('.footerlist').append(
-            "<li><a href='http://cloud0477.dothome.co.kr/'><img src='https://res.cloudinary.com/dgggcrkxq/image/upload/v1573198836/noticon/uydcemefupin4kvi5pii.gif'> 개발자 사이트</a></li>");
+            "<li><a href='http://cloud0477.dothome.co.kr/'>개발자 사이트</a></li>");
 
         $('.footerlist').children(':last').css({
             'margin':'5px'
         });
 
         $('.footerlist').children(':last').children('a').children('img').css({
-            'width':'15px'
+            'width':'15px',
+            'display' :'inline'
         });
     });
 });
